734f3cf2b3f65cfbb6c258d2c8b814c69ee6089d
Christian Dean <christianodean@gmail.com>
2015-03-08 05:52:37 +0000

    kelton review

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,22 @@
-class StaticPagesController < ApplicationController
-  def home
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
-  def help
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/...'
-
   get 'static_pages/home'
-
   get 'static_pages/help'
- 
+  get 'static_pages/about'
+
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
