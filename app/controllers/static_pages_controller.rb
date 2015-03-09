0cde72b94e6c81766051c6299b57619016fdb2e6
Christian Dean <christianodean@gmail.com>
2015-03-09 05:31:06 +0000

    fixed issue

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,11 @@
-require 'test_helper'
+class StaticPagesController < ApplicationController
 
-class StaticPagesControllerTest < ActionController::TestCase
-
-  test "should get home" do
-    get :home
-    assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  def home
   end
 
-  test "should get help" do
-    get :help
-    assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  def help
   end
 
-  test "should get about" do
-    get :about
-    assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  def about
   end
 end
